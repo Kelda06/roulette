b3003b9b0b20db9eda9fad1be940ba7e0f285e8a
Adam Keller <adam.bugatti@post.cz>
2023-09-21 20:30:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+repeat = "yes"
+cash = int(input("Enter the amount with which you will bet in roulette: "))
+while repeat == "yes":
+    
+
+    print("Your current available amount is: ")
+    print(cash)
+
+    num = random.randint(1, 37)
+    if num == 37:
+        clr = "green"
+
+    else:
+        if num % 2 == 1:
+            clr = "red"
+
+        elif num % 2 == 0:
+            clr = "black"
+
+
+    print(clr)
+    s_c = (input("Choose color you want to bet on (red or black): "))
+    s_a = int(input("Choose ammount you want to bet: "))
+
+    if s_a > cash:
+        print("You are brokie hahaha!")
+        break
+
+    print("The color is " + clr)
+
+    if s_c == clr:
+        cash = cash + s_a
+        print("You have lucky... Your current ammount is: ")
+        print(cash)
+    else:
+        money = cash - s_a
+        print("You lost your bet looser! Your current ammount is: ")
+        print(cash)
+
+
+
+
+    repeat = (input("Next chance?  (Yes bcs I'am gambler/No bcs I'am smart) "))
